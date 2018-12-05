77be3153ca4a2f90acffa00a20e55c7c7d41c277
vishwa <vishwanathovi@gmail.com>
2018-12-05 19:24:09 +0530

    minor css changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,9 @@ function displayCells(){
 	for (let i = 0;i<20;i++){
 		cellContent += `<div class="row">`
 		for (let j = 0;j<26;j++){
-			if (i==0){ cellContent += `<div class="column-headers">${String.fromCharCode(j+65)}</div>` ;continue;}
 			if (j==0){ cellContent += `<div class="row-headers">${i}</div>` ;continue;}
+			if (i==0){ cellContent += `<div class="column-headers">${String.fromCharCode(j+65-1)}</div>` ;continue;}
+
 			id = String.fromCharCode(j+65-1) + (i);
 			cellContent += `<input type="text" id="${id}"> `
 		}
