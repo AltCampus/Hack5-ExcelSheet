e28c8256e5b071e7545ae06d8d1cea5928629f49
vishwa <vishwanathovi@gmail.com>
2018-12-05 16:43:12 +0530

    cell grid creation

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+displayCells();
+
+function displayCells(){
+	var cellSection = document.querySelector('.cell-section');
+	var cellContent = "";
+	var id ="";
+
+	for (let i = 0;i<20;i++){
+		cellContent += `<div class="row">`
+		for (let j = 0;j<26;j++){
+			if (i==0){ cellContent += `<div class="column-headers">${String.fromCharCode(j+65)}</div>` ;continue;}
+			if (j==0){ cellContent += `<div class="row-headers">${i}</div>` ;continue;}
+			id = String.fromCharCode(j+65) + (i);
+			cellContent += `<input type="text" id="${id}"> `
+		}
+		cellContent += `</div>`
+	}
+	cellSection.innerHTML = cellContent;
+}
